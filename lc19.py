849275d81457e5625936f569cd417ff195fdace4
gledunca <gledunca@amazon.com>
2022-11-03 14:00:00 -0800

    R3 -- LC19

---
+++ lc19.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        a, b = head, head
+        for _ in range(n):
+            b = b.next
+        if not b:
+            return a.next
+        while b.next:
+            a = a.next
+            b = b.next
+        a.next = a.next.next
+        return head
