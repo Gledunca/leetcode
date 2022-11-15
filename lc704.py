2aa4e6e0122334b12d4a45f4745251aa369ba8bf
gledunca <gledunca@amazon.com>
2022-11-15 14:00:00 -0800

    R9 -- LC704

---
+++ lc704.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums)-1
+        while i <= j:
+            k = (i+j) // 2
+            if nums[k] == target:
+                return k
+            elif nums[k] < target:
+                i = k+1
+            else:
+                j = k-1
+        return -1
